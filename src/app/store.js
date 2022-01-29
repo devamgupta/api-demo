8a3f40edf3698f8777e790959649a0e170c791d0
devamgupta <devamgupta26@gmail.com>
2022-01-29 20:10:06 +0530

    Added redux-logger middleware to the redux store using configureStore function of redux-toolkit.

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import menuItemReducer from '../features/menuItem/menuItemSlice';
+import logger from 'redux-logger';
 
 export default configureStore({
   reducer: {
       menuItem: menuItemReducer
-  }
+  },
+  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(logger)
 })
