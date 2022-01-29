ec5ca3f9f46c2dd21c177e7d431daef30ae51a05
devamgupta <devamgupta26@gmail.com>
2022-01-29 20:06:27 +0530

    Using redux-toolkit + redux-thunk.(not using createAsyncThunk and  extraReducers)

--- src/features/menuItem/menuItemSlice.js
+++ src/features/menuItem/menuItemSlice.js
@@ -1,28 +1,23 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit'
 import axios from 'axios'
 
 const API_URL = 'https://api.dotpe.in/api/catalog/store/1/menu?mDomain=fryerstory.in&saletype=delivery&serviceSubtype=delivery'
 
-export const fetchMenuItems = createAsyncThunk(
-    'menuItem/fetchMenuItems',
-    async (args, thunkAPI) => {
-        const { dispatch, getState } = thunkAPI;
-        dispatch(fetchMenuItemsRequest())
-        return await (
-            axios.get(API_URL)
-            .then(response => {
-                const data = response.data;
-                const items = Object.keys(data.menuItems).map(function(key, index) {
-                    return data.menuItems[key];
-                })
-                dispatch(fetchMenuItemsSuccess(items));
-            })
-            .catch(error => {
-                dispatch(fetchMenuItemsFailure(error.message));
-            })
-        )
-    }
-)
+
+export const fetchMenuItems = () => dispatch => {
+  dispatch(fetchMenuItemsRequest())
+  axios.get(API_URL)
+  .then(response => {
+      const data = response.data;
+      const items = Object.keys(data.menuItems).map(function(key, index) {
+          return data.menuItems[key];
+      })
+      dispatch(fetchMenuItemsSuccess(items));
+  })
+  .catch(error => {
+      dispatch(fetchMenuItemsFailure(error.message));
+  })
+}
 
 export const menuItemSlice = createSlice({
   name: 'menuItem',
